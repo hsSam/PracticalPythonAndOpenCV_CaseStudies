6faa75645a1befe1a3a8c3bbb11e17b193a2a1ad
Thomas Reaney <thomas@reaney.net>
2018-01-22 10:47:13 +0000

    Chapter02: Face Detection

--- Chapter02/detect_faces.py
+++ Chapter02/detect_faces.py
@@ -1,34 +1,23 @@
-# USAGE
-# python detect_faces.py --face cascades/haarcascade_frontalface_default.xml --image images/obama.png
-
-# import the necessary packages
-from __future__ import print_function
-from pyimagesearch.facedetector import FaceDetector
-import argparse
+from utilities.facedetector import FaceDetector
 import cv2
 
-# construct the argument parse and parse the arguments
-ap = argparse.ArgumentParser()
-ap.add_argument("-f", "--face", required = True,
-	help = "path to where the face cascade resides")
-ap.add_argument("-i", "--image", required = True,
-	help = "path to where the image file resides")
-args = vars(ap.parse_args())
+# Define paths
+image_path = 'images/obama.png'
+cascade_path = 'cascades/haarcascade_frontalface_default.xml'
 
-# load the image and convert it to grayscale
-image = cv2.imread(args["image"])
+# Load the image and convert it to greyscale
+image = cv2.imread(image_path)
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-# find faces in the image
-fd = FaceDetector(args["face"])
-faceRects = fd.detect(gray, scaleFactor = 1.1, minNeighbors = 5,
-	minSize = (30, 30))
-print("I found {} face(s)".format(len(faceRects)))
+# Find faces in the image
+detector = FaceDetector(cascade_path)
+face_boxes = detector.detect(gray, 1.2, 5)
+print("{} face(s) found".format(len(face_boxes)))
 
-# loop over the faces and draw a rectangle around each
-for (x, y, w, h) in faceRects:
-	cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
+# Loop over the faces and draw a rectangle around each
+for (x, y, w, h) in face_boxes:
+    cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
-# show the detected faces
+# Show the detected faces
 cv2.imshow("Faces", image)
 cv2.waitKey(0)
